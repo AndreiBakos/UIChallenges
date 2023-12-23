6a54ff701cf9b235491fcc4b1ea4f16bff50289f
Andrei Bakos <andreib347@yahoo.com>
2023-12-23 12:35:39 +0200

    Initial commit

---
+++ UIChallenges/UIChallenges/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIChallenges
+//
+//  Created by Andrei Bakos on 23.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world2!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIChallenges/UIChallenges/UIChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIChallengesApp.swift
+//  UIChallenges
+//
+//  Created by Andrei Bakos on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
